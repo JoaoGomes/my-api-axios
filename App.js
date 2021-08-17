27ab6ae07e36facd65d79ac2fb21c21a5c354f4c
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-17 14:20:07 -0300

    Atualizando GET e POST.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { StyleSheet, Text, View, Button, FlatList } from "react-native";
 
@@ -11,20 +11,48 @@ const Item = ({ fullName }) => (
 export default function App() {
 	const [data, setData] = useState([]);
 	const [error, setError] = useState(false);
+  const [postStatus, setPostStatus] = useState('');
 
-	function onPressAPIGet () {
-		axios({
-			method: 'get',
-			url: 'https://reqresERRO.in/api/users'
-	})
-	.then(response => {
-		setData(response.data.data);
-	})
-	.catch(err => {
-		setError(true);
-		console.log(err);
-	});
-}
+  async function onPressAPIPost() {
+    
+    const user = {
+      first_name: "John",
+      last_name: "Lilly",
+      job_title: "Software Engineer",
+    };
+
+    try {
+      const response = await
+      axios.post('https://reqres.in/api/users');
+      setPostStatus(response.status);
+      console.log(response);
+    }
+    catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  }
+	
+  async function onPressAPIGet () {
+		try {
+      const response = await
+      axios.get('https://reqres.in/api/users');
+
+      if(response.data.data.length > 1) {
+        setData(response.data.data);
+	    }
+      else {
+        const arrayResponse = [];
+        arrayResponse.push(response.data.data);
+        setData(arrayResponse);
+      }
+    }
+    
+    catch(err) {
+		  setError(true);
+		  console.log(err);
+	  }
+  }
 
 const renderItem = ({ item }) => <Item fullName={item.first_name + ' ' + item.last_name} />;
 
@@ -32,11 +60,15 @@ return (
 	<View style={styles.container}>
 		<Text style={styles.header}>Teste de API</Text>
 		<Button onPress={onPressAPIGet} title="API Get" color="#841584" />
-	  {error && <Text style={styles.error}>Ocorreu um erro ao chamar a API</Text>}
+    <Button onPress={onPressAPIPost} title="API Post" color="#459809" />
+    <Text>Status POST: {postStatus}</Text>
+    {error && <Text style={styles.error}>Ocorreu um erro ao chamar a API</Text>}
 	  <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id.toString()} />
 	</View>
 );
 }
+
+
 
 const styles = StyleSheet.create({
 	container: {
